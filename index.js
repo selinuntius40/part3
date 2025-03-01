cbbc9d89386ed7f821d84c028e853a03bf02bfec
selinuntius40 <120080805+Selinuntius02@users.noreply.github.com>
2025-03-01 22:45:54 +1000

    exercise 3.18

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ morgan.token('content', function (req, res) {return req.method === 'POST' ? JSON
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id' })
+    }
+
+    next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {     
         response.json(persons)
@@ -34,17 +48,22 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-	Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+	Person.findById(request.params.id)
+    .then(person => {
         response.json(person)
     })
+    .catch(error => next(error))
+    
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
 
-	Person.findByIdAndDelete(request.params.id).then(result => {
+	Person.findByIdAndDelete(request.params.id)
+    .then(result => {
         response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -77,6 +96,23 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
